1fb275045e77abc7c7cf1bcb1676c017faa0342b
lianfanghua <fabrice_lian@163.com>
2021-01-10 22:00:21 +0800

    新增功能： 1、增加接口方法；

--- lib/picture_rotation_fix.dart
+++ lib/picture_rotation_fix.dart
@@ -1,14 +1,17 @@
-
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 
 class PictureRotationFix {
-  static const MethodChannel _channel =
-      const MethodChannel('picture_rotation_fix');
+  static const MethodChannel _channel = const MethodChannel('picture_rotation_fix');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<Uint8List> fixOrientation(String src, int compressQuality) async {
+    return _channel.invokeMethod('fix', {'src': src, 'quality': compressQuality});
   }
 }
